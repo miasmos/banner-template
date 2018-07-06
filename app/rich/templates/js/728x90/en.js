0a485d2fec87572980c448d9ff632cab723a0f31
Stephen Poole <stephenwpoole@gmail.com>
2018-07-06 12:49:56 -0400

    Completed 300x250, 300x600, 728x90 EN & FR. Added statics.

--- app/rich/templates/js/728x90/en.js
+++
@@ -1,1 +0,0 @@
-console.log('this js is in every 728x90 en creative');
---
+++ app/rich/templates/js/animations/fr/300x250.js
@@ -0,0 +1,49 @@
+var repeatCount = 0;
+var repeatTotal = 1;
+var timeline = new TimelineMax({repeat: repeatTotal});
+
+timeline
+    .add('frame1')
+    .to('.copy1', 0.5, {opacity: 1})
+    .addDelay(3, 'frame2');
+timeline
+    .add('frame2')
+    .to('.copy1,.background1', 0.5, {opacity: 0})
+    .to('.copy2,.background2', 0.5, {opacity: 1})
+    .addDelay(1, 'frame2-copy')
+    .to('.copy3', 0.5, {opacity: 1})
+    .add('frame2-copy')
+    .addDelay(3, 'frame3');
+timeline
+    .add('frame3')
+    .to('.copy2,.copy3,.background2', 0.5, {opacity: 0})
+    .to(
+        '.background3,.copy4,.copy5,.details,.legal-trigger,.legal-exit-trigger',
+        0.5,
+        {
+            opacity: 1
+        }
+    )
+    .to('.cta', 0, {top: '20px'})
+    .to('.cta', 0.5, {opacity: 1})
+    .addDelay(2, 'frame3-copy')
+    .to('.copy5,.cta', 0.5, {opacity: 0})
+    .to('.copy4', 0.5, {top: '-52px'}, '-=0.5')
+    .to('.cta', 0, {top: 0})
+    .add('frame3-copy')
+    .to('.copy6,.details,.cta', 0.5, {opacity: 1})
+    .addDelay(5, 'frame4')
+    .call(() => {
+        if (repeatCount++ >= repeatTotal) {
+            timeline.pause();
+        }
+    });
+timeline
+    .add('frame4')
+    .to('.copy4,.copy6,.details,.background3,.cta', 0.5, {
+        opacity: 0
+    })
+    .to('.background1', 0.5, {
+        opacity: 1
+    });
+timeline.add('frame5');
