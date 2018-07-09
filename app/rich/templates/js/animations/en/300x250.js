93864790fd336dc391491aea039936903cb2a845
Stephen Poole <stephenwpoole@gmail.com>
2018-07-09 09:51:59 -0400

    Reduced time interval between each frame.

--- app/rich/templates/js/animations/en/300x250.js
+++ app/rich/templates/js/animations/en/300x250.js
@@ -1,22 +1,22 @@
 var repeatCount = 0;
 var repeatTotal = 1;
-var timeline = new TimelineMax({repeat: repeatTotal});
+var timeline = new TimelineMax({ repeat: repeatTotal });
 
 timeline
     .add('frame1')
-    .to('.copy1', 0.5, {opacity: 1})
-    .addDelay(3, 'frame2');
+    .to('.copy1', 0.5, { opacity: 1 })
+    .addDelay(2, 'frame2');
 timeline
     .add('frame2')
-    .to('.copy1,.background1', 0.5, {opacity: 0})
-    .to('.copy2,.background2', 0.5, {opacity: 1})
+    .to('.copy1,.background1', 0.5, { opacity: 0 })
+    .to('.copy2,.background2', 0.5, { opacity: 1 })
     .addDelay(1, 'frame2-copy')
-    .to('.copy3', 0.5, {opacity: 1})
+    .to('.copy3', 0.5, { opacity: 1 })
     .add('frame2-copy')
-    .addDelay(3, 'frame3');
+    .addDelay(2, 'frame3');
 timeline
     .add('frame3')
-    .to('.copy2,.copy3,.background2', 0.5, {opacity: 0})
+    .to('.copy2,.copy3,.background2', 0.5, { opacity: 0 })
     .to(
         '.background3,.copy4,.copy5,.details,.legal-trigger,.legal-exit-trigger',
         0.5,
@@ -24,12 +24,12 @@ timeline
             opacity: 1
         }
     )
-    .to('.cta', 0.5, {opacity: 1})
+    .to('.cta', 0.5, { opacity: 1 })
     .addDelay(2, 'frame3-copy')
-    .to('.copy5', 0.5, {opacity: 0})
-    .to('.copy4', 0.5, {top: '-40px'}, '-=0.5')
+    .to('.copy5', 0.5, { opacity: 0 })
+    .to('.copy4', 0.5, { top: '-40px' }, '-=0.5')
     .add('frame3-copy')
-    .to('.copy6,.details', 0.5, {opacity: 1})
+    .to('.copy6,.details', 0.5, { opacity: 1 })
     .addDelay(5, 'frame4')
     .call(() => {
         if (repeatCount++ >= repeatTotal) {
