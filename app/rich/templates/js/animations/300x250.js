2b131c45347d71bd02e020eba0a2e6f713531128
Stephen Poole <stephenwpoole@gmail.com>
2018-09-18 14:49:02 -0400

    Completed 300x250, 728x90, 300x600.

--- app/rich/templates/js/animations/300x250.js
+++ app/rich/templates/js/animations/300x250.js
@@ -1,5 +1,5 @@
-var repeatCount = 0;
 var repeatTotal = 1;
+var repeatCount = 0;
 var timeline = new TimelineMax({ repeat: repeatTotal });
 
 timeline
@@ -8,38 +8,35 @@ timeline
     .addDelay(2, 'frame2');
 timeline
     .add('frame2')
-    .to('.background1', 0.5, { opacity: 0 })
+    .to('.copy1,.background1', 0.5, { opacity: 0 })
     .to('.copy2,.background2', 0.5, { opacity: 1 })
+    .addDelay(1, 'frame2-copy')
+    .to('.copy3', 0.5, { opacity: 1 })
+    .add('frame2-copy')
     .addDelay(2, 'frame3');
 timeline
     .add('frame3')
-    .to('.background2', 0.5, { opacity: 0 })
-    .to('.copy3,.background3', 0.5, { opacity: 1 })
-    .add('frame2-copy')
-    .addDelay(2, 'frame4');
-timeline
-    .add('frame4')
-    .to('.copy1,.copy2,.copy3,.background2,.background3', 0.5, { opacity: 0 })
+    .to('.copy2,.copy3,.background2', 0.5, { opacity: 0 })
     .to(
-        '.background4,.copy4,.details,.legal-trigger,.legal-exit-trigger',
+        '.background3,.copy4,.copy5,.details,.legal-trigger,.legal-exit-trigger',
         0.5,
         {
             opacity: 1
         }
     )
     .to('.cta', 0.5, { opacity: 1 })
-    .addDelay(3, 'frame5')
+    .addDelay(5, 'frame4')
     .call(() => {
         if (repeatCount++ >= repeatTotal) {
             timeline.pause();
         }
     });
 timeline
-    .add('frame5')
-    .to('.copy4,.details,.background4,.cta', 0.5, {
+    .add('frame4')
+    .to('.copy4,.details,.background3,.cta', 0.5, {
         opacity: 0
     })
     .to('.background1', 0.5, {
         opacity: 1
     });
-timeline.add('frame6');
+timeline.add('frame5');
